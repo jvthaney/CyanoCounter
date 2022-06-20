db735f57133943b8d3e46cc1476723539a981b69
Jessica Trout-Haney <70969187+jvthaney@users.noreply.github.com>
2022-06-20 19:35:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -81,21 +81,6 @@ min8.addEventListener('click', minCounter8);
 reset.addEventListener('click', resetCounter);
 exp.addEventListener('click', tableToCSV);
 
-
-//Ccalculate cells per sample
-// var gradcylinder = gradoutput.innerHTML/1000
-// var cellsperslide = counter_num * 4
-// var percentcounted = 1/gradcylinder
-// var cellspersample = cellsperslide/percentcounted
-//Then volume lake water
-// var diameter = diaminput.innerHTML
-// var depth = depthinput.innerHTML
-// var lakewater = Math.PI * (diameter/2)^2 * depth
-//Then cells per volume lake water
-// var cellsperlake = cellspersample/lakewater
-
-
-
 //////////Add-button functions
 function addCounter1(){
     //First add counter # to table
@@ -630,3 +615,103 @@ function fn1(){
 }
 
 
+// Get the modal for image 1
+var modal1 = document.getElementById("myModal1");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var modal5 = document.getElementById("myModal5");
+var modal6 = document.getElementById("myModal6");
+var modal7 = document.getElementById("myModal7");
+var modal8 = document.getElementById("myModal8");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+var img6 = document.getElementById("myImg6");
+var img7 = document.getElementById("myImg7");
+var img8 = document.getElementById("myImg8");
+
+var modalImg1 = document.getElementById("img01");
+var modalImg2 = document.getElementById("img02");
+var modalImg3 = document.getElementById("img03");
+var modalImg4 = document.getElementById("img04");
+var modalImg5 = document.getElementById("img05");
+var modalImg6 = document.getElementById("img06");
+var modalImg7 = document.getElementById("img07");
+var modalImg8 = document.getElementById("img08");
+
+var captionText1 = document.getElementById("caption1");
+var captionText2 = document.getElementById("caption2");
+var captionText3 = document.getElementById("caption3");
+var captionText4 = document.getElementById("caption4");
+var captionText5 = document.getElementById("caption5");
+var captionText6 = document.getElementById("caption6");
+var captionText7 = document.getElementById("caption7");
+var captionText8 = document.getElementById("caption8");
+
+img1.onclick = function(){
+  modal1.style.display = "block";
+  modalImg.src = this.src;
+  captionText1.innerHTML = this.alt;}
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText2.innerHTML = this.alt;}
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+  captionText3.innerHTML = this.alt;}
+img4.onclick = function(){
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+  captionText4.innerHTML = this.alt;}
+img5.onclick = function(){
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+  captionText5.innerHTML = this.alt;}
+img6.onclick = function(){
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+  captionText6.innerHTML = this.alt;}
+img7.onclick = function(){
+  modal7.style.display = "block";
+  modalImg7.src = this.src;
+  captionText7.innerHTML = this.alt;}
+img8.onclick = function(){
+  modal8.style.display = "block";
+  modalImg8.src = this.src;
+  captionText8.innerHTML = this.alt;}
+
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+var span3 = document.getElementsByClassName("close3")[0];
+var span4 = document.getElementsByClassName("close4")[0];
+var span5 = document.getElementsByClassName("close5")[0];
+var span6 = document.getElementsByClassName("close6")[0];
+var span7 = document.getElementsByClassName("close7")[0];
+var span8 = document.getElementsByClassName("close8")[0];
+
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";}
+span2.onclick = function() {
+    modal2.style.display = "none";}
+span3.onclick = function() {
+    modal3.style.display = "none";}
+span4.onclick = function() {
+    modal4.style.display = "none";}
+span5.onclick = function() {
+    modal5.style.display = "none";}
+span6.onclick = function() {
+    modal6.style.display = "none";}
+span7.onclick = function() {
+    modal7.style.display = "none";}
+span8.onclick = function() {
+    modal8.style.display = "none";}
